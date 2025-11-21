b14fdc6393079f64bb5bc20428d4c11d4720ef4f
光鱼 <guang.yu@yingdao.com>
2025-11-21 17:54:48 +0800

    Add VSCode settings for Prettier formatting and implement message sending functionality in the AppController with a new utility for message formatting.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { sendMessage } from './utils/message';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('message')
+  async sendMessage(@Body() body: any) {
+    await sendMessage(JSON.stringify(body, null, 2));
   }
 }
---
+++ src/utils/message.ts
@@ -0,0 +1,53 @@
+const resportUrl =
+  'https://open.feishu.cn/open-apis/bot/v2/hook/cb513eed-cdc7-4afd-ba2b-fb788bf2ac07';
+
+const getMessageData = (
+  infos: {
+    title: string;
+    content: string;
+  }[],
+) => {
+  const elements = infos.map((info) => {
+    return {
+      tag: 'div',
+      text: {
+        content: `${info.title} : ${info.content}`,
+        tag: 'lark_md',
+      },
+    };
+  });
+  return {
+    msg_type: 'interactive',
+    card: {
+      config: {
+        wide_screen_mode: true,
+        enable_forward: true,
+      },
+      header: {
+        template: 'green',
+        title: {
+          content: 'AI Review',
+          tag: 'plain_text',
+        },
+      },
+      elements,
+    },
+  };
+};
+
+export async function sendMessage(message: string) {
+  await fetch(resportUrl, {
+    method: 'POST',
+    body: JSON.stringify(
+      getMessageData([
+        {
+          title: 'AI Review',
+          content: message,
+        },
+      ]),
+    ),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
