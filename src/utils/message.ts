3d0fc0d9d14272953f0b59fb6ad7703d1efd868e
光鱼 <guang.yu@yingdao.com>
2025-11-24 16:39:22 +0800

    refactor: remove getMessageData function from message utility to streamline code

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,40 +1,6 @@
 const resportUrl =
   'https://open.feishu.cn/open-apis/bot/v2/hook/cb513eed-cdc7-4afd-ba2b-fb788bf2ac07';
 
-const getMessageData = (
-  infos: {
-    title: string;
-    content: string;
-  }[],
-) => {
-  const elements = infos.map((info) => {
-    return {
-      tag: 'div',
-      text: {
-        content: `${info.title} : ${info.content}`,
-        tag: 'lark_md',
-      },
-    };
-  });
-  return {
-    msg_type: 'interactive',
-    card: {
-      config: {
-        wide_screen_mode: true,
-        enable_forward: true,
-      },
-      header: {
-        template: 'green',
-        title: {
-          content: 'AI Review',
-          tag: 'plain_text',
-        },
-      },
-      elements,
-    },
-  };
-};
-
 export async function sendMdMessage(title: string, message: string) {
   await fetch(resportUrl, {
     method: 'POST',
